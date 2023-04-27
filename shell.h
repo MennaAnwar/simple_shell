30029f389cdd32309db0bec1bce30034ed16d43b
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-04-27 04:42:38 +0200

    fix build

--- shell.h
+++ shell.h
@@ -158,7 +158,7 @@ void free_info(info_t *, int);
 char *_getenv(info_t *, const char *);
 int _env(info_t *);
 int _mysetenv(info_t *);
-int _mysetenv(info_t *);
+int _myunsetenv(info_t *info);
 int _unsetenv(info_t *, char *);
 int _setenv(info_t *, char *, char *);
 int env_list(info_t *);
