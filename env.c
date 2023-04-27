c025eacf4349a0aec96985e3fe3a1f30f536fbef
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-04-27 03:54:47 +0200

    env

--- env.c
+++ env.c
@@ -1,15 +1,15 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
+ * _env - prints the current environment
  * @info: Structure containing potential arguments. Used to maintain
  *		  constant function prototype.
  * Return: Always 0
  */
 int _env(info_t *info)
 {
-	print_list_str(info->env);
-	return (0);
+    print_list_str(info->env);
+    return (0);
 }
 
 /**
@@ -20,14 +20,14 @@ int _env(info_t *info)
  */
 size_t print_list_str(const list_t *h)
 {
-	size_t i = 0;
+    size_t i = 0;
 
-	while (h)
-	{
-		_puts(h->str ? h->str : "(nil)");
-		_puts("\n");
-		h = h->next;
-		i++;
-	}
-	return (i);
+    while (h)
+    {
+        _puts(h->str ? h->str : "(nil)");
+        _puts("\n");
+        h = h->next;
+        i++;
+    }
+    return (i);
 }
