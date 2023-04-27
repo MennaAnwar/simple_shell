d0b33eab76fd23a40ca0c0adeb18cadc3684be7e
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2023-04-27 04:18:24 +0200

    read cmds from file

--- shell.c
+++ shell.c
@@ -7,10 +7,33 @@
  *
  * Return: 0 on success, 1 on error
  */
-int main(__attribute__((unused)) int argc, char **agrv)
+int main(int argc, char **agrv)
 {
-	info_t info[] = { INFO_INIT };
+	info_t info[] = {INFO_INIT};
+	int fp = 2;
 
+	fp += 3;
+
+	if (argc == 2)
+	{
+		fp = open(argv[1], O_RDONLY);
+		if (fp == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_error_puts(av[0]);
+				_error_puts(": 0: Can't open ");
+				_error_puts(av[1]);
+				_error_putchar('\n');
+				_error_putchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfp = fp;
+	}
 	hsh(info, agrv);
 	return (EXIT_SUCCESS);
 }
